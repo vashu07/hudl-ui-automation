bf23888d60ed497f9b656b572d2ef579b166c328
vashu07 <varshaprabhakarm07@gmail.com>
2025-08-11 17:30:02 +0100

    Added WaitUtils

--- src/test/java/com/hudl/utils/ScrollUtils.java
+++ src/test/java/com/hudl/utils/ScrollUtils.java
@@ -2,9 +2,14 @@ package com.hudl.utils;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.TimeoutError;
+import com.microsoft.playwright.options.LoadState;
+
+import java.util.logging.Logger;
 
 public class ScrollUtils {
 
+    private static final Logger logger = Logger.getLogger(ScrollUtils.class.getName());
 
     /** Scroll a specific element into view */
     public static void scrollToElement(Page page, String selector) {
@@ -17,7 +22,7 @@ public class ScrollUtils {
      * @param scrollStep pixels to scroll each time (e.g., 500)
      * @param maxScrolls max number of scroll attempts to avoid infinite loops
      * @param waitMs wait time in milliseconds after each scroll
-     * TODO: con
+     * TODO: Improve this to consider newHeight
      */
     public static Locator infiniteScrollInContainer(Page page, String containerSelector, int scrollStep, int maxScrolls, int waitMs) {
         Locator container = page.locator(containerSelector);
@@ -28,4 +33,5 @@ public class ScrollUtils {
         }
         return container;
     }
+
 }
